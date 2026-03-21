dbf7b1e725e4dab90746cc466e7c085724614625
websheriff <websheriff@fastmail.com>
2026-03-21 18:10:35 -0500

    remove line from home/sops.nix not needed

--- home/core/sops.nix
+++ home/core/sops.nix
@@ -8,7 +8,6 @@
     age.keyFile = "/home/websheriff/.config/sops/age/keys.txt";
 
     defaultSopsFile = ../../secrets.yaml;
-    #validateSopsFile = false;
 
     secrets."users/websheriff/private_key" = {
       path = "/home/websheriff/.ssh/id_ed25519";
